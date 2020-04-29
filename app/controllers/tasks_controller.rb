8c7f8f18606524851ec5a9f1c5bcbc62d15d7d81
moriwo <m.forest0000@gmail.com>
2020-04-29 12:08:31 +0000

    l.15 kadai revision3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:show, :new, :create, :edit, :destroy, :update]
+  before_action :correct_user, only: [:show, :edit, :destroy, :update]
   
   def index
     if logged_in?
