5273ca25ab197cd90489a777dfd25c2966649ac7
moriwo <m.forest0000@gmail.com>
2020-04-29 09:34:47 +0000

    l.15 kadai revision1

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  get 'toppages/index'
+ # get 'sessions/new'
+ # get 'sessions/create'
+ # get 'sessions/destroy'
   get 'users/index'
   get 'users/show'
-  get 'users/new'
+  #get 'users/new'
   get 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
@@ -15,8 +14,8 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  
-   get 'signup', to: 'users#new'
+
+  get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
   
 end
