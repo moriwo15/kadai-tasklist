6f722c3ad2daff3c0e64ed4c13a73d25090afcb5
moriwo <m.forest0000@gmail.com>
2020-04-29 11:17:35 +0000

    l.15 kadai revision2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :new, :create, :edit, :destroy, :update]
   
   def index
     if logged_in?
@@ -27,8 +28,10 @@ class TasksController < ApplicationController
       render :edit
     end
   end
+  
   def edit
   end
+  
   def update
     
     if @task.update(task_params)
@@ -39,6 +42,7 @@ class TasksController < ApplicationController
       render :edit
     end
   end
+  
   def destroy
     @task.destroy
     
@@ -47,11 +51,20 @@ class TasksController < ApplicationController
   end
   
   private
+  
   def set_task
     @task = Task.find(params[:id])
   end
+  
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
 
