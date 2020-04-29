93e150d8c2909c6dc6f80f23ed28ee697dd58c39
moriwo <m.forest0000@gmail.com>
2020-04-29 07:38:40 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -9,14 +9,14 @@ Rails.application.routes.draw do
   get 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
+  
   resources :tasks
-  get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
-  get 'signup', to: 'users#new'
+   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
+  
 end
